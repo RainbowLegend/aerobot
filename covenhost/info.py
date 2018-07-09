47e3f839b3f88747b48d9358473c68ce10de3625
RainbowLegend <rainbowlegend3054@gmail.com>
2018-07-09 16:23:16 -0400

    Create info.py

---
+++ covenhost/info.py
@@ -0,0 +1,9 @@
+{
+    "AUTHOR" : "RainbowLegend",
+    "SHORT" : "Coven hosting cogs",
+    "DESCRIPTION" : "",
+    "DISABLED" : false,
+    "NAME" : "CovenHost",
+    "TAGS" : ["rcogs"],
+    "INSTALL_MSG" : "Updated/Installed."
+}
