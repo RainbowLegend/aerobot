1848400071b7e6075555d00fcf28d59499c4ec1f
RainbowLegend <rainbowlegend3054@gmail.com>
2018-07-10 20:26:33 -0400

    [py] Fixed args issue
    embed.add_field requires both a name and a value, only a name was provided. Fixed by putting into a general description.

--- ruleloader/ruleloader.py
+++ ruleloader/ruleloader.py
@@ -128,9 +128,9 @@ class RuleLoader:
       """Rule loading embed""" 
       icon = ctx.message.server.icon_url
       embed = discord.Embed(0x9B0029)
-      embed.set_author(name='Section 5: Final Note',icon_url=icon)
-      embed.add_field(name='*If you believe someone is breaching the rules, please contact staff either in a direct message or the channel it happened.*',inline=False)
-      embed.add_field(name='*If a staff member is in breach of their rules, please DM an Administrator.*',inline=False)
+      embed.set_author(name='Section 5: Final Note', icon_url=icon, description="""**If you believe someone is breaching the rules, please contact staff either in a direct message or the channel it happened.**
+      
+      *If a staff member is in breach of their rules, please DM an Administrator.*""")
       embed.set_footer(text='If you have any question about the rules, please feel free to DM a Staff Member.',icon_url=icon)
         
       await self.bot.say(embed=embed)
