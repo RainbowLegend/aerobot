b4b43f7fdc0d80361f744e1df5e8c782a84ce6b0
elijahnt <40532634+elijahnt@users.noreply.github.com>
2018-10-30 21:29:25 -0600

    Create newemotes.py

---
+++ newemotes/newemotes.py
@@ -0,0 +1,12 @@
+import discord
+
+class NewEmotes:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if ":dab:" in message.content or ";dab;" in message.content:
+            await self.bot.send_message(message.channel, “<a:eli1:472941813011972107><a:eli2:472941813229944842><a:eli3:472941813267824670>")
+
+def setup(bot):
+    bot.add_cog(EmotesLmao(bot))
