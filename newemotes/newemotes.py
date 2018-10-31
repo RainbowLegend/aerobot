0d733c379554ffdba0ef3ef0bec1a349832e88d8
elijahnt <40532634+elijahnt@users.noreply.github.com>
2018-10-30 21:41:24 -0600

    Update newemotes.py

--- newemotes/newemotes.py
+++ newemotes/newemotes.py
@@ -5,7 +5,7 @@ class NewEmotes:
         self.bot = bot
 
     async def on_message(self, message):
-        if ":dab:" in message.content or ";dab;" in message.content:
+        if "!dab" in message.content or ";dab;" in message.content:
             await self.bot.send_message(message.channel, "<a:eli1:472941813011972107><a:eli2:472941813229944842><a:eli3:472941813267824670>")
 
 def setup(bot):
