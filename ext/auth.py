c213aa55ce8fe89ce1f465ba4b7ac3b1ed8edc4f
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-06 12:22:59 -0500

    updootdate

---
+++ ext/auth.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+
+class Auth:
+    """Authorization command for the official TOSCD."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command
+    async def auth(self, ctx):
+        destination = self.bot.get_channel(288677420720979978)
+        inv = await self.bot.create_invite(destination, max_uses=1, max_age=60)
+        await ctx.author.send(f'Your unique invite link is: {inv}')
+
+
+def setup(bot):
+    bot.add_cog(Auth(bot))
--- ext/misc.py
+++ ext/misc.py
@@ -4,7 +4,7 @@ import random
 from config import lelijah
 
 
-class Miscellaneous:
+class Misc:
     def __init__(self, bot):
         self.bot = bot
 
@@ -12,14 +12,14 @@ class Miscellaneous:
     async def penis(self, ctx, user: discord.Member=None):
         """:b:enis?"""
         if user.id == 228700305263558656:
-            return await self.bot.say(lelijah)
+            return await ctx.send(lelijah)
         elif user is None:
             user = ctx.author
         state = random.getstate()
         random.seed(user.id)
         random.setstate(state)
-        await self.bot.say(f"Size: 8{'=' * random.randint(0, 30)}D")
+        await ctx.send(f"Size: 8{'=' * random.randint(0, 30)}D")
 
 
 def setup(bot):
-    bot.add_cog(Miscellaneous(bot))
+    bot.add_cog(Misc(bot))
