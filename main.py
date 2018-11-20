71f1549c697100419c6909c0c980ccb1b40bfa7d
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-19 21:25:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import config
+import logging
 
 
 class AeroBot(commands.Bot):
@@ -19,5 +20,11 @@ bot.load_extension('jishaku')
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
+    
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 bot.run(config.token)
