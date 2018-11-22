faac460e127cae6b9d22b29876995f2af7b27761
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-21 19:40:32 -0500

    Update cleverbot.py

--- ext/cleverbot.py
+++ ext/cleverbot.py
@@ -31,6 +31,9 @@ class CleverBot:
             }
 
             self.context[str(message.author.id)].append(text)
+            
+            if len(self.context[str(message.author.id]) > 2:
+                self.context[str(message.author.id)].pop(0)
 
             async with message.channel.typing(), aiohttp.ClientSession() as cs:
                 req = await cs.post("https://public-api.travitia.xyz/talk", json=payload,
