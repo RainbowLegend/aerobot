41badbc21c0b01dd3007bb8cf787cab9a1399a19
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-26 14:01:24 -0500

    Create _travis.py

---
+++ _travis.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import logging
+import os
+class Config:
+    token = os.getenv("TOKEN")
+    cogs = ['ext.auth', 'ext.cleverbot', 'ext.hosting', 'ext.ir_emb', 'ext.jokinator', 'ext.misc', 'ext.moderation', 'ext.owner', 'ext.tosrole']
+config = Config
+
+class AeroBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def load_extensions(self, extensions):
+        for extension in extensions:
+            self.load_extension(extension)
+
+
+bot = AeroBot(command_prefix='/', case_insensitive=True)
+bot.load_extensions(config.extensions)
+bot.load_extension('jishaku')
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+print("Travis Tests Successful!")
