9608a88ce6d3302279a6d4be28726ccca535cfcc
RainbowLegend <rainbowlegend3054@gmail.com>
2018-12-18 20:08:33 -0500

    Update owner.py

--- ext/owner.py
+++ ext/owner.py
@@ -124,6 +124,7 @@ class Owner:
         """Restarts the bot"""
         await ctx.send(embed=discord.Embed(color=0x00FFFF, description="wow lowkey rude but fine"))
         os.execl(sys.executable, sys.executable, * sys.argv)
+        await ctx.send("argggg looks like im back")
 
 
 def setup(bot):
