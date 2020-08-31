61b006079e1a1776d7590cb80df49330e905b292
RainbowLegend <rainbowlegend3054@gmail.com>
2020-08-31 01:13:55 -0400

    PEP8 update
    remove unnecessary whitespace, change class descriptions to docstring

--- ext/jokinator.py
+++ ext/jokinator.py
@@ -5,15 +5,13 @@ from consts import JOKES
 
 
 class Jokinator(commands.Cog):
-
-    # Let's make jokes!
+    """Let's make jokes!"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="dadjoke")
     async def dadjoke(self, ctx):
-
         async with aiohttp.ClientSession() as cs:
             async with cs.get('https://icanhazdadjoke.com/', headers={"Accept": "application/json"}) as r:
                 raw_joke = await r.json()
@@ -22,14 +20,11 @@ class Jokinator(commands.Cog):
 
     @commands.command(name="givejoke", aliases=["joke"])
     async def givejoke(self, ctx):
-
         await ctx.send(f'{ctx.message.author.mention}, {random.choice(JOKES)}')
 
+    # This command is used for the dabs.
     @commands.command(name="dab")
     async def dab(self, ctx):
-
-      # This command is used for the dabs.
-
       ctx.send("<a:Aerobot_Dab_1:749791197224108043><a:Aerobot_Dab_2:749791187313098764><a:Aerobot_Dab_3:749791176063975504>")
 
 
