b1b4f9ae7c2cbcaffa26fefa46b0e35f91921e78
RainbowLegend <rainbowlegend3054@gmail.com>
2020-08-31 01:18:24 -0400

    code style update

--- ext/owner.py
+++ ext/owner.py
@@ -26,7 +26,7 @@ class Owner(commands.Cog):
     @commands.command(hidden=True, name='eval', aliases=['evaluate'])
     @commands.is_owner()
     async def _eval(self, ctx, *, body: str):
-        # Evaluates a piece of code
+        """Evaluates a piece of code"""
         env = {
             'bot': self.bot,
             'ctx': ctx,
@@ -112,7 +112,7 @@ class Owner(commands.Cog):
     @commands.command(name="runas", hidden=True, aliases=["impersonate"])
     @commands.is_owner()
     async def runas(self, ctx, member: discord.Member, *, cmd):
-        # Invoke bot command as specified user.
+        """Invoke bot command as specified user."""
         msg = copy.copy(ctx.message)
         msg.content = f"{ctx.me.mention} {cmd}"
         msg.author = member
@@ -121,7 +121,7 @@ class Owner(commands.Cog):
     @commands.command(name="restart", aliases=["reboot"], hidden=True)
     @commands.is_owner()
     async def restart(self, ctx):
-        # Restarts the bot
+        """Restarts the bot"""
         await ctx.send("Restarting...")
         await self.bot.logout()
         raise KeyboardInterrupt
