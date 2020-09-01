96a16ae6daf97cb0f7e5478b22fdb311f383f1d9
mystlegend <rainbowlegend3054@gmail.com>
2020-09-01 11:29:20 -0400

    Add new names and json db

--- ext/nitro.py
+++ ext/nitro.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import json
 from discord.ext import commands
 
 
@@ -27,20 +28,47 @@ class NitroPerks(commands.Cog):
         await newrole.edit(position=self.guild.roles.index(ghost_role))
         await asyncio.sleep(1)
         await ctx.author.add_roles(newrole, reason="Nitro Booster Role")
+        with open("nitro.json") as f:
+            db = json.load(f)
+            db[str(ctx.author.id)] = newrole.id
+            json.dump(db, f)
         return await ctx.send("Your new role has been created!")
 
     @customrole.command(name="color")
     async def color(self, ctx, *, hexinput=None):
-        if not discord.utils.get(self.guild.roles, name=str(ctx.author.id)):
+        with open("nitro.json") as f:
+            db = json.load(f)
+        if not discord.utils.get(self.guild.roles, name=str(ctx.author.id)) or str(ctx.author.id) not in db.keys():
             return await ctx.send("Create a role before using this command!")
 
-        userrole = discord.utils.get(self.guild.roles, name=str(ctx.author.id))
+        user_role = discord.utils.get(self.guild.roles, name=str(ctx.author.id)) \
+                    or self.guild.get_role(db[str(ctx.author.id)])
+
+        if str(ctx.author.id) not in db.keys():
+            db[str(ctx.author.id)] = user_role.id
 
         try:
-            await userrole.edit(color=discord.Color(value=int(hexinput.strip("#"), 16)))
+            await user_role.edit(color=discord.Color(value=int(hexinput.strip("#"), 16)))
             return await ctx.send(f"You have changed your role's color to {hexinput}!")
         except ValueError:
             return await ctx.send("Input a valid hex code such as `#FFFFFF`!")
+
+    @customrole.command(name="name")
+    async def name(self, ctx, *, name):
+        with open("ntiro.json") as f:
+            db = json.load(f)
+        if not discord.utils.get(self.guild.roles, name=str(ctx.author.id)) or str(ctx.author.id) not in db.keys():
+            return await ctx.send("Create a role before using this command!")
+
+        user_role = discord.utils.get(self.guild.roles, name=str(ctx.author.id)) \
+                    or self.guild.get_role(db[str(ctx.author.id)])
+
+        if str(ctx.author.id) not in db.keys():
+            db[str(ctx.author.id)] = user_role.id
+
+        await user_role.edit(name=name)
+        return await ctx.send(f"Your role's name has been changed to **{name}**")
+
 
 def setup(bot):
     bot.add_cog(NitroPerks(bot))
