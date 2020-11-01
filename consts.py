c3e8ec076df2018171ff37bc57660ebca9ac0689
RainbowLegend <rainbowlegend3054@gmail.com>
2020-11-01 00:31:52 -0400

    Move emotes to new server

--- consts.py
+++ consts.py
@@ -1,45 +1,45 @@
 CLASSIC = (
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370>\n"
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589>\n"
     "<@&702609120540491786> **||** We're voting for today's gamemodes! "
     "React to an emoji to vote.\nYou may choose more than 1.\n\n"
     "**You may choose from these options:**\n"
-    "<:Classic_Classic:723204741278204006> - Classic.\n"
-    "<:Classic_RankedPractice:723204796945006643> - Ranked Practice.\n"
-    "<:Classic_Custom:723204754431279125> - Custom.\n"
-    "<:Classic_RapidMode:723204808336736388> - Rapid Mode.\n"
-    "<:Classic_AllAny:723204714132668498>- All Any.\n"
-    "<:Classic_Rainbow:723204782369538088> - Rainbow.\n"
-    "<:Classic_DraculasPalace:723204769111605350> - Dracula's Palace.\n"
-    "<:Classic_TownTraitor:723204820718321745> - Town Traitor."
+    "<:Classic_Classic:772315471277981706> - Classic.\n"
+    "<:Classic_RankedPractice:772315471164211221> - Ranked Practice.\n"
+    "<:Classic_Custom:772315471381921812> - Custom.\n"
+    "<:Classic_RapidMode:772315471289647124> - Rapid Mode.\n"
+    "<:Classic_AllAny:772315471323856908>- All Any.\n"
+    "<:Classic_Rainbow:772315471432253450> - Rainbow.\n"
+    "<:Classic_DraculasPalace:772315471424389120> - Dracula's Palace.\n"
+    "<:Classic_TownTraitor:772315471378513930> - Town Traitor."
 )
 
 COVEN = (
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370>\n"
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589>\n"
     "<@&702610491717189673> **||** We're voting for today's coven gamemodes! "
     "React to an emoji to vote.\nYou may choose more than 1.\n\n"
     "**You may choose from these options:**\n"
-    "<:Coven_Classic:723204853433761893> - Classic.\n"
-    "<:Coven_RankedPractice:723204889332809821> - Ranked Practice.\n"
-    "<:Coven_MafiaReturns:723204877458604156> - Mafia Returns.\n"
-    "<:Coven_Custom:723204865639055370> - Custom.\n"
-    "<:Coven_AllAny:723204840729346119>- All Any.\n"
-    "<:Coven_Rotating:723204902121373747> - Rotating Gamemode."
+    "<:Coven_Classic:772315471235776513> - Classic.\n"
+    "<:Coven_RankedPractice:772315471248228352> - Ranked Practice.\n"
+    "<:Coven_MafiaReturns:772315471202091029> - Mafia Returns.\n"
+    "<:Coven_Custom:772315471285452810> - Custom.\n"
+    "<:Coven_AllAny:772315471332376616>- All Any.\n"
+    "<:Coven_Rotating:772315471235645461> - Rotating Gamemode."
 )
 
 VOTE = (
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370>\n"
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589>\n"
     "<@&702609120540491786> **+** <@&702610491717189673> **||** We're voting the type of gamenight! "
     "React to an emoji to vote.\nYou may choose more than 1.\n\n"
     "**You may choose from these options:**\n"
@@ -48,22 +48,22 @@ VOTE = (
 )
 
 START=(
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370>\n"
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589>\n"
     '{0.mention} **||** A new game of **{1}** is starting.\n\n'
     'Use `/joingame [ToS IGN]` or `/jg [ToS IGN]` to join. You will shortly get a party '
     'invite.'
 )
 
 FINAL=(
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370> <a:Gamenight_Party:723208543116984370> "
-    "<a:Gamenight_Party:723208543116984370>\n"
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589> <a:Gamenight_Party:772315471265005589> "
+    "<a:Gamenight_Party:772315471265005589>\n"
     '{0.mention} **||** A new game of **{1}** is starting.\nThe game will start shortly.\n\n'
     'Use `/joingame [ToS IGN]` or `/jg [ToS IGN]` to join. You will shortly get a party '
     'invite.'
